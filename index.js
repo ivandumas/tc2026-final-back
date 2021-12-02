f6f4775b0275a079cbe025889e4b63018a3eb7a7
Iván Alejandro Dumas Martínez <ivan.dumas@hotmail.com>
2021-12-01 21:34:22 -0600

    Update index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+//Importando la biblioteca para realizar aplicaciones web
+const express = require("express")
+const path = require("path")
+
+const dRoutes = require('./routes/divisiones')
+const eRoutes = require('./routes/equipos')
+const sequelize = require('./utils/database')
+
+//Creación de la aplicación web
+const app = express();
+
+//Middleware
+app.use(express.static(path.join(__dirname,'public')))
+//Configurar el servidor y que sepa que es un json
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.use("/equipos",eRoutes);
+app.use("/divisiones",dRoutes);
+
+//Que la aplicación escuche peticiones
+sequelize.sync()
+    .then(()=>{app.listen(8080,()=>{
+        console.log("Aplicación web en línea en el puerto 8080")
+    })})
+    .catch()
